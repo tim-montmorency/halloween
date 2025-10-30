77719f44df3b9012ebdfa7055c7ca746c2c00bb1
Thomas O Fredericks <thomas@fredericks.info>
2025-10-30 09:54:00 -0400

    Update artist credits in French localization

--- locales/fr.js
+++ locales/fr.js
@@ -16,7 +16,7 @@ export const fr = {
   ],
   "more-info": "<a href='https://tim-montmorency.com/' target='_blank' rel='noopener noreferrer'>Pour plus d’information sur le programme TIM</a>",
   "section-credits-title": "Crédits",
-  "credits-artists": "Student artists: Émeryk Bélisle, Ahmed K, Ting Yung Terry Lu, Rafael Angon Dube, Ryan Dufault, Elie Daher, Kristy Moussally, Marie-Lili Laprise, Yannick Chamberland, Mégane Ranger, Benjamin Ferland, Dana Saavedra-Torrano, Matis Ghariani, Mikael Atseneau, Amira Tounekti, Mathieu Willett, Justin Montpetit, Kordan Radhouane, prénom nom, Alexandre Gendron, Jade Hébert",
+  "credits-artists": "Student artists: Émeryk Bélisle, Ahmed K, Ting Yung Terry Lu, Rafael Angon Dube, Ryan Dufault, Elie Daher, Kristy Moussally, Marie-Lili Laprise, Yannick Chamberland, Mégane Ranger, Benjamin Ferland, Dana Saavedra-Torrano, Matis Ghariani, Mikael Atseneau, Amira Tounekti, Mathieu Willett, Justin Montpetit, Kordan Radhouane, Thearylou Lach, Alexandre Gendron, Jade Hébert",
   "credits-collaborators": "Professeurs: Guillaume Arseneault et Thomas Ouellet Fredericks",
   "credits-projection": "Projection mapping: Init & XPAV",
   "label-partenaires": "Partenaires",
